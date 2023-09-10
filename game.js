c96294cda7155157fc3a431820cbcd8ca4b68b11
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2023-09-11 00:46:14 +0530

    user input and cordination with sound done!

--- game.js
+++ game.js
@@ -1,16 +1,31 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
 
-let  buttonColours=["red", "blue", "green", "yellow"]
+let gamePattern = [];
+let userClickedPattern = [];
 
- let gamePattern= [];
-
-function nextSequence(){
-    let randomNumber = Math.floor(Math.random()*4);
-    let randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-$(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
-let sound = new Audio (`./sounds/${randomChosenColour}.mp3`);
-    sound.play();
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  
 }
-
 nextSequence();
 // $(`#${gamePattern}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  $(this).fadeIn(100).fadeOut(100).fadeIn(100)
+ playSound(userChosenColour);
+});
+
+
+function playSound(name) {
+
+    let sound = new Audio(`./sounds/${name}.mp3`);
+    sound.play();
+    
+}
