b9c52dffde2c040cd3fb6f5c27a36745685906b9
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2023-09-12 01:44:56 +0530

    starting the game and changin the head tag

--- game.js
+++ game.js
@@ -2,38 +2,45 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$("body").keypress(function () {
+  if (!started) {
+    $("#level-title").html(`level ${level}`);
+
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
 function nextSequence() {
+  level++;
+  $("#level-title").html(`level ${level}`);
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  
 }
-nextSequence();
-// $(`#${gamePattern}`).fadeIn(100).fadeOut(100).fadeIn(100);
-
-$(".btn").click(function () {
-  let userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
- playSound(userChosenColour);
- animatePress(userChosenColour);
-});
-
 
 function playSound(name) {
-
-    let sound = new Audio(`./sounds/${name}.mp3`);
-    sound.play();
-    
+  let sound = new Audio(`./sounds/${name}.mp3`);
+  sound.play();
 }
 
-function animatePress(currentColour){
-    $(`#${currentColour}`).addClass("pressed")
+function animatePress(currentColour) {
+  $(`#${currentColour}`).addClass("pressed");
 
-    setTimeout(() => {
-      $(`#${currentColour}`).removeClass("pressed")
-    }, 100);
+  setTimeout(() => {
+    $(`#${currentColour}`).removeClass("pressed");
+  }, 100);
 }
